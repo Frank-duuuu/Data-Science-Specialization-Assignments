2d4846fd98728a00f68b4a0a4c5b3491305930a1
Frank DU <frankdu0207@gmail.com>
2020-07-20 22:30:54 +0800

    Initial Commit

---
+++ Getting and Cleaning Data/Course Project/run_analysis.R
@@ -0,0 +1,45 @@
+# Reading files.
+library(dplyr)
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+# “You should create one R script called run_analysis.R that does the following:”
+
+# Step 1: Merges the training and the test sets to create one data set.
+x <- rbind(x_train, x_test)
+y <- rbind(y_train, y_test)
+subject <- rbind(subject_train, subject_test)
+merged_data <- cbind(subject, y, x)
+
+# Step 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+tidy_data <- select(merged_data, subject, code, contains('mean'), contains('std'))
+
+# Step 3: Uses descriptive activity names to name the activities in the data set.
+tidy_data$code <- activities[tidy_data$code, 2]
+
+# Step 4: Appropriately labels the data set with descriptive variable names.
+names(tidy_data)[2] <- 'activity'
+names(tidy_data)<-gsub("Acc", "Accelerometer", names(tidy_data))
+names(tidy_data)<-gsub("Gyro", "Gyroscope", names(tidy_data))
+names(tidy_data)<-gsub("BodyBody", "Body", names(tidy_data))
+names(tidy_data)<-gsub("Mag", "Magnitude", names(tidy_data))
+names(tidy_data)<-gsub("^t", "Time", names(tidy_data))
+names(tidy_data)<-gsub("^f", "Frequency", names(tidy_data))
+names(tidy_data)<-gsub("tBody", "TimeBody", names(tidy_data))
+names(tidy_data)<-gsub("-mean()", "Mean", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("-std()", "STD", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("-freq()", "Frequency", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("angle", "Angle", names(tidy_data))
+names(tidy_data)<-gsub("gravity", "Gravity", names(tidy_data))
+
+# Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+FinalData <- tidy_data %>%
+        group_by(subject, activity) %>%
+        summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name = FALSE)
